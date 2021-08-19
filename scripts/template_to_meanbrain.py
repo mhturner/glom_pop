986acf9439fc97b5c001a92e48efd43e8a02b2ff
Max Turner <maxwellholteturner@gmail.com>
2021-08-19 14:15:09 -0700

    Align template to meanbrain using sytGCamp signal instead

--- scripts/template_to_meanbrain.py
+++ scripts/template_to_meanbrain.py
@@ -41,10 +41,10 @@ lobe_mask = ants.from_numpy(np.squeeze(lobe_mask), spacing=meanbrain_red.spacing
 
 atlas_spacing = (0.38, 0.38, 0.38)  # um
 template = np.squeeze(np.asanyarray(nib.load(os.path.join(base_dir, 'template_brain', 'jrc2018.nii')).dataobj).astype('uint32'))
-glom_mask = np.squeeze(np.asanyarray(nib.load(os.path.join(base_dir, 'template_brain', 'vpn_glom_mask_closed_eroded.nii')).dataobj).astype('uint32'))
+glom_mask = np.squeeze(np.asanyarray(nib.load(os.path.join(base_dir, 'template_brain', 'vpn_glom_mask_closed.nii')).dataobj).astype('uint32'))
 
-# Remove mask voxels == 1 (LC10, not in volume really, messes with synthmorph)
-glom_mask[glom_mask==1] = 0
+# Remove mask voxels == 1 (LC10, not in volume, messes with synthmorph)
+glom_mask[glom_mask == 1] = 0
 
 template = ants.from_numpy(template, spacing=atlas_spacing)
 glom_mask = ants.from_numpy(glom_mask, spacing=atlas_spacing)
@@ -61,52 +61,25 @@ ax = ax.ravel()
 for z in range(18):
     ax[z].imshow(template_smoothed[:, :, z*5].T)
 
-# %% SHOW MEAN BRAIN
-meanbrain_smoothed = dataio.get_smooth_brain(meanbrain_red, smoothing_sigma=[2, 2, 1])
+# %% SHOW MEAN BRAIN - GREEN. Align on this.
+meanbrain_smoothed = dataio.get_smooth_brain(meanbrain_green, smoothing_sigma=[2, 2, 1])
 
 fh, ax = plt.subplots(4, 5, figsize=(16, 8))
 ax = ax.ravel()
 [x.set_axis_off() for x in ax.ravel()]
 for z in range(18):
     ax[z].imshow(meanbrain_smoothed[:, :, z*2].T)
-
-# %% COMPUTE ALIGNMENT - affine only
-
-reg = ants.registration(meanbrain_smoothed,  # fixed = meanbrain
-                        template_smoothed,  # Moving = smoothed template
-                        type_of_transform='Affine',
-                        mask=lobe_mask,  # mask includes only gloms, excludes lobe
-                        flow_sigma=6,
-                        total_sigma=1,
-                        random_seed=1)
-
-# APPLY ALIGNMENT TO (RAW) MASK & TEMPLATE
-template_transformed = ants.apply_transforms(meanbrain_red,
-                                             template,
-                                             reg['fwdtransforms'],
-                                             interpolator='nearestNeighbor')
-
-glom_mask_transformed = ants.apply_transforms(meanbrain_red,
-                                              glom_mask,
-                                              reg['fwdtransforms'],
-                                              interpolator='genericLabel')
-
-# # Save transformed brains and mask
-nib.save(nib.Nifti1Image(glom_mask_transformed.numpy(), np.eye(4)), os.path.join(base_dir, 'aligned', 'glom_mask_affine2meanbrain.nii'))
-nib.save(nib.Nifti1Image(template_transformed.numpy(), np.eye(4)), os.path.join(base_dir, 'aligned', 'JRC2018_affine2meanbrain.nii'))
-
 # %% COMPUTE ALIGNMENT - SyN
 
 # TODO: make this more repeatable across runs?
 #   os.environ['ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS'] should be set to "1" as well, I think. But this slows everything down a lot
-reg = ants.registration(meanbrain_smoothed,  # fixed = meanbrain
+reg = ants.registration(meanbrain_smoothed,  # fixed = meanbrain, green channel
                         template_smoothed,  # Moving = smoothed template
                         type_of_transform='SyN',
                         mask=lobe_mask,  # mask includes only gloms, excludes lobe
                         flow_sigma=6,
                         total_sigma=0,
                         random_seed=1)
-# TODO: save transforms and re-load from permanent dir in apply step
 
 # APPLY ALIGNMENT TO MASK & TEMPLATE
 template_transformed = ants.apply_transforms(meanbrain_red,
@@ -133,7 +106,7 @@ for x in ax.ravel():
 
 # %%
 # Show z slices of meanbrain, template, & glom map for alignment
-z_levels = [5, 10, 15, 20, 25, 30]
+z_levels = [5, 10, 15, 20, 25, 30, 35]
 
 glom_size_threshold = 300
 
@@ -150,26 +123,28 @@ for m_ind, mask_id in enumerate(vals):
     else:
          pass
 
-
 cmap = cc.cm.glasbey
 colors = cmap(vals/vals.max())
 norm = mcolors.Normalize(vmin=0, vmax=vals.max(), clip=True)
 glom_tmp = np.ma.masked_where(glom_mask_transformed.numpy()==0, glom_mask_transformed.numpy()) # mask at 0
+grn_tmp = np.ma.masked_where(lobe_mask.numpy()==0, meanbrain_green.numpy()) # mask at 0
 
-fh, ax = plt.subplots(len(z_levels), 3, figsize=(6, 12))
+fh, ax = plt.subplots(len(z_levels), 4, figsize=(8, 12))
 [x.set_xticklabels([]) for x in ax.ravel()]
 [x.set_yticklabels([]) for x in ax.ravel()]
 [x.tick_params(bottom=False, left=False) for x in ax.ravel()]
 
 for z_ind, z in enumerate(z_levels):
     ax[z_ind, 0].imshow(meanbrain_red[:, :, z].T, cmap='Reds')
-    ax[z_ind, 1].imshow(template_transformed[:, :, z].T, cmap='Blues')
-    ax[z_ind, 2].imshow(glom_tmp[:, :, z].T, cmap=cmap, norm=norm, interpolation='none')
+    ax[z_ind, 1].imshow(grn_tmp[:, :, z].T, cmap='Greens')
+    ax[z_ind, 2].imshow(template_transformed[:, :, z].T, cmap='Blues')
+    ax[z_ind, 3].imshow(glom_tmp[:, :, z].T, cmap=cmap, norm=norm, interpolation='none')
 
     if z_ind==0:
-        ax[z_ind, 0].set_title('mtdTomato meanbrain')
-        ax[z_ind, 1].set_title('JRC2018')
-        ax[z_ind, 2].set_title('Glomerulus map')
+        ax[z_ind, 0].set_title('mtdTomato')
+        ax[z_ind, 1].set_title('syt1GCaMP6F')
+        ax[z_ind, 2].set_title('JRC2018')
+        ax[z_ind, 3].set_title('Glomerulus map')
 
         dx = 25 / 1.0 # um -> pix
         ax[z_ind, 0].plot([45, 45+dx], [180, 180], color='k', linestyle='-', marker='None', linewidth=2)
