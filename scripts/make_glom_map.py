fa4e6781fc230c4a2d472528786e84bfdbb55c8f
Max Turner <maxwellholteturner@gmail.com>
2021-08-19 13:41:56 -0700

    Small transform to match glom map -> template Used morpho-closed density map to register to JRC2018. Slight movement but really does clean up alignment between map and template

--- scripts/make_glom_map.py
+++ scripts/make_glom_map.py
@@ -14,6 +14,9 @@ from skimage import morphology
 import colorcet as cc
 import matplotlib.colors as mcolors
 from matplotlib.patches import Patch
+import ants
+
+from glom_pop import dataio
 
 base_dir = '/Users/mhturner/Dropbox/ClandininLab/Analysis/glom_pop'
 
@@ -43,57 +46,87 @@ brain_mask = np.flip(brain_mask[410:645, 250:450, 230:340], axis=2)
 brain_density = np.flip(brain_density[410:645, 250:450, 230:340], axis=2)
 print('Trimmed brain_mask shape = {}'.format(brain_mask.shape))
 
-
 # Load template brain
 template = np.squeeze(np.asanyarray(nib.load(os.path.join(base_dir, 'template_brain', 'JRC2018_FEMALE_38um_iso_16bit.nii')).dataobj).astype('uint32')) # xyz
 print('Full template shape = {}'.format(template.shape))
 template = np.flip(template[410:645, 250:450, 230:340], axis=2)
 print('Trimmed template shape = {}'.format(template.shape))
 
-
-# Load mask key for VPN types
-vpn_types = pd.read_csv(os.path.join(base_dir, 'template_brain', 'vpn_types.csv'))
-
-# Save trimmed template & raw mask
+# Save trimmed template & mask (pre-transformation)
 nib.save(nib.Nifti1Image(template, np.eye(4)), os.path.join(base_dir, 'template_brain', 'jrc2018.nii'))
 nib.save(nib.Nifti1Image(brain_mask, np.eye(4)), os.path.join(base_dir, 'template_brain', 'vpn_glom_mask.nii'))
 nib.save(nib.Nifti1Image(brain_density, np.eye(4)), os.path.join(base_dir, 'template_brain', 'vpn_glom_density.nii'))
+# %% Register density -> template
 
+# Convert to ANTS images
+atlas_spacing = (0.38, 0.38, 0.38)  # um
+
+template = ants.from_numpy(template, spacing=atlas_spacing)
+brain_density = ants.from_numpy(brain_density, spacing=atlas_spacing)
+brain_mask = ants.from_numpy(brain_mask, spacing=atlas_spacing)
+
+# registration images: Smoothed template and closed density map
+template_smoothed = dataio.get_smooth_brain(template, smoothing_sigma=[3, 3, 2])
+brain_density_closed = morphology.closing(brain_density.numpy() > 0, selem=morphology.ball(4))
+brain_density_closed = brain_density_closed * 1.0  # bool -> float
+brain_density_closed = ants.from_numpy(brain_density_closed, spacing=atlas_spacing)
+
+# %% Register density -> template
+
+reg = ants.registration(template_smoothed,  # fixed = template, nc82, smoothed
+                        brain_density_closed,  # Moving = syn density, closed
+                        type_of_transform='SyN',
+                        flow_sigma=6,
+                        total_sigma=0,
+                        random_seed=1)
+
+# Apply alignment to brain density and mask
+brain_density_transformed = ants.apply_transforms(template,
+                                                  brain_density,
+                                                  reg['fwdtransforms'],
+                                                  interpolator='nearestNeighbor')
+
+brain_mask_transformed = ants.apply_transforms(template,
+                                               brain_mask,
+                                               reg['fwdtransforms'],
+                                               interpolator='genericLabel')
+
+# Save transformed mask & density
+nib.save(nib.Nifti1Image(brain_mask_transformed.numpy(), np.eye(4)), os.path.join(base_dir, 'template_brain', 'vpn_glom_mask_transformed.nii'))
+nib.save(nib.Nifti1Image(brain_density_transformed.numpy(), np.eye(4)), os.path.join(base_dir, 'template_brain', 'vpn_glom_density_transformed.nii'))
 
 # %% MORPHOLOGICAL OPERATIONS ON EACH GLOMERULUS MASK
 
-mask_ids = np.unique(brain_mask)[1:]  # exclude first (=0, i.e. nothing)
+# Back to numpy arrays, don't need ants anymore
+brain_mask_transformed = brain_mask_transformed.numpy()
 
-closed_mask = np.zeros_like(brain_mask)
-closed_eroded_mask = np.zeros_like(brain_mask)
+mask_ids = np.unique(brain_mask_transformed)[1:]  # exclude first (=0, i.e. nothing)
+
+closed_eroded_mask = np.zeros_like(brain_mask_transformed)
 for mask_id in mask_ids:
-    morph_mask = morphology.closing(brain_mask == mask_id, selem=morphology.ball(4))
-    closed_mask[morph_mask] = mask_id
-
+    # Closing
+    morph_mask = morphology.closing(brain_mask_transformed == mask_id, selem=morphology.ball(4))
+    # One more erosion to remove speckles
     morph_mask = morphology.binary_erosion(morph_mask, selem=morphology.ball(1))
     closed_eroded_mask[morph_mask] = mask_id
 
-# Save closed masks
-nib.save(nib.Nifti1Image(closed_mask, np.eye(4)), os.path.join(base_dir, 'template_brain', 'vpn_glom_mask_closed.nii'))  # morpho closed each glom
-nib.save(nib.Nifti1Image(closed_eroded_mask, np.eye(4)), os.path.join(base_dir, 'template_brain', 'vpn_glom_mask_closed_eroded.nii'))  # morpho closed each glom
+# Save closed mask
+nib.save(nib.Nifti1Image(closed_eroded_mask, np.eye(4)), os.path.join(base_dir, 'template_brain', 'vpn_glom_mask_closed.nii'))
 
 # %% SHOW
 z_slice = 60
 
-norm = mcolors.Normalize(vmin=1, vmax=brain_mask.max(), clip=True)
+norm = mcolors.Normalize(vmin=1, vmax=brain_mask_transformed.max(), clip=True)
 
-fh, ax = plt.subplots(1, 4, figsize=(18, 6))
+fh, ax = plt.subplots(1, 3, figsize=(18, 6))
 ax[0].imshow(template[:, :, z_slice].T, cmap='Blues')
 ax[0].set_title('JRC2018')
 
-ax[1].imshow(np.ma.masked_where(brain_mask == 0, brain_mask)[:, :, z_slice].T, cmap=cc.cm.glasbey, norm=norm, interpolation='none')
-ax[1].set_title('Raw Density Map')
+ax[1].imshow(np.ma.masked_where(brain_mask_transformed == 0, brain_mask_transformed)[:, :, z_slice].T, cmap=cc.cm.glasbey, norm=norm, interpolation='none')
+ax[1].set_title('Density Map')
 
-ax[2].imshow(np.ma.masked_where(closed_mask == 0, closed_mask)[:, :, z_slice].T, cmap=cc.cm.glasbey, norm=norm, interpolation='none')
+ax[2].imshow(np.ma.masked_where(closed_eroded_mask == 0, closed_eroded_mask)[:, :, z_slice].T, cmap=cc.cm.glasbey, norm=norm, interpolation='none')
 ax[2].set_title('Closed Map')
-
-ax[3].imshow(np.ma.masked_where(closed_eroded_mask == 0, closed_eroded_mask)[:, :, z_slice].T, cmap=cc.cm.glasbey, norm=norm, interpolation='none')
-ax[3].set_title('Closed, Eroded Map')
 
 
 for x in ax.ravel():
