90e858a5a0f3e24ae1cb3cc393fc833af2bdd05f
Max Turner <maxwellholteturner@gmail.com>
2022-03-18 20:02:29 -0700

    sh

--- moco/moco_brainsss.sh
+++ moco/moco_brainsss.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#SBATCH --mail-type=END,FAIL
 #SBATCH --job-name=moco
 #SBATCH --partition=trc
 #SBATCH --time=4-00:00:00
@@ -15,9 +16,11 @@ SERIES_BASE=$2
 BRAIN_MASTER="${SERIES_BASE}_channel_1.nii"
 BRAIN_MIRROR="${SERIES_BASE}_channel_2.nii"
 
+DATA_DIRECTORY="${}moco"
+
 ml python/3.6 antspy/0.2.2
 
-args="{\"directory\":\"$DIRECTORY\",\"brain_master\":\"$BRAIN_MASTER\",\"brain_mirror\":\"$BRAIN_MIRROR\"}"
+args="{\"directory\":\"$DATA_DIRECTORY\",\"brain_master\":\"$BRAIN_MASTER\",\"brain_mirror\":\"$BRAIN_MIRROR\"}"
 
 python3 -u /home/users/mhturner/brainsss/scripts/motion_correction.py $args
 python3 -u /home/users/mhturner/glom_pop/moco/h5_to_nii.py "${DIRECTORY}${SERIES_BASE}"
