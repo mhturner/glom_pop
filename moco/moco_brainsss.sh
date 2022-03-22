86029eedc5482225127c0de42b2372612f062e6b
Max Turner <maxwellholteturner@gmail.com>
2022-03-22 10:38:45 -0700

    meanbrain_n_frames for moco_brainsss sh

--- moco/moco_brainsss.sh
+++ moco/moco_brainsss.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=/home/users/mhturner/glom_pop/moco/%x.%j.out
 #SBATCH --open-mode=append
 
-# Params: (1) base directory, (2) series base name (no suffixes)
+# Params: (1) base directory, (2) series base name (no suffixes) (3) optional - type_of_transform (4) optional - meanbrain_n_frames
 # USAGE: sbatch moco_brainsss.sh /oak/stanford/groups/trc/data/Max/ImagingData/Bruker/.../ TSeries-2022MMDD-00n
 
 date
@@ -24,7 +24,10 @@ echo $brain_mirror
 
 # Optional params
 type_of_transform="${3:-"Rigid"}"
+meanbrain_n_frames = "${4:-"100"}"
+
 output_format="nii"
+echo $meanbrain_n_frames
 echo $type_of_transform
 echo $output_format
 
@@ -33,7 +36,7 @@ moco_directory="${directory}/moco/"
 ml python/3.6 antspy/0.2.2
 
 args="{\"directory\":\"$directory\",\"brain_master\":\"$brain_master\",\"brain_mirror\":\"$brain_mirror\","\
-"\"type_of_transform\":\"$type_of_transform\",\"output_format\":\"$output_format\"}"
+"\"type_of_transform\":\"$type_of_transform\",\"output_format\":\"$output_format\",\"meanbrain_n_frames\":\"$meanbrain_n_frames\"}"
 
 python3 -u /home/users/mhturner/brainsss/scripts/motion_correction.py $args
 python3 -u /home/users/mhturner/glom_pop/moco/merge_channels.py ${moco_directory}${series_base}
