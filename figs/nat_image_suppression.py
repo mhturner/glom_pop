337fc95012d72d811e9dba00068b0c73c729b633
Max Turner <maxwellholteturner@gmail.com>
2022-04-11 13:46:34 -0700

    nat image suppression

--- figs/nat_image_suppression.py
+++ figs/nat_image_suppression.py
@@ -5,12 +5,15 @@ import numpy as np
 import os
 from glom_pop import dataio, util
 from scipy.stats import ttest_rel
+from skimage.io import imread
 
 util.config_matplotlib()
 
 sync_dir = dataio.get_config_file()['sync_dir']
 save_directory = dataio.get_config_file()['save_directory']
 transform_directory = os.path.join(sync_dir, 'transforms', 'meanbrain_template')
+images_dir = os.path.join(dataio.get_config_file()['images_dir'], 'vh_tif')
+
 
 leaves = np.load(os.path.join(save_directory, 'cluster_leaves_list.npy'))
 included_gloms = dataio.get_included_gloms()
@@ -24,14 +27,12 @@ matching_series = shared_analysis.filterDataFiles(data_directory=os.path.join(sy
                                                                        'indicator_2': 'TdTomato'},
                                                   target_series_metadata={'protocol_ID': 'NaturalImageSuppression',
                                                                           'include_in_analysis': True,
+                                                                          'image_speed': [-40.,   0.,  40.,  80., 120.],
                                                                           })
 
 # %%
 
-# shape = gloms, images, filter conditions, flies, time
-all_responses = np.zeros( (len(included_gloms), 5, 2, len(matching_series), 43) )
-all_responses[:] = np.nan
-all_responses.shape
+all_responses = []
 for s_ind, series in enumerate(matching_series):
     series_number = series['series']
     file_path = series['file_name'] + '.hdf5'
@@ -44,33 +45,50 @@ for s_ind, series in enumerate(matching_series):
     epoch_response_matrix = dataio.filter_epoch_response_matrix(response_data, included_vals)
 
     # Align responses
-    parameter_key = ('stim0_image_name', 'current_filter_flag')
+    parameter_key = ['stim0_image_name', 'current_filter_flag', 'current_image_speed']
     unique_parameter_values, mean_response, sem_response, trial_response_by_stimulus = ID.getTrialAverages(epoch_response_matrix, parameter_key=parameter_key)
-    image_names = np.unique([x.replace('whitened_', '') for x in unique_parameter_values[:, 0]])
-    filter_codes = [0, 1]  # 0=raw, 1=whitened, 2=DoG
-    for f_ind, filter_code in enumerate(filter_codes):
-        for im_ind, image_name in enumerate(image_names):
-            pull_ind = np.where(np.logical_and([image_name in x for x in unique_parameter_values[:, 0]],
-                                               [filter_code == float(x) for x in unique_parameter_values[:, 1]]))[0][0]
-            all_responses[:, im_ind, f_ind, s_ind, :] = mean_response[:, pull_ind, :]
+
+    all_responses.append(mean_response)
 
 # %%
-all_responses.shape
-image_names
-fh, ax = plt.subplots(len(included_gloms), 5, figsize=(4, 8))
-[x.set_ylim([-0.1, 0.35]) for x in ax.ravel()]
-[util.clean_axes(x) for x in ax.ravel()]
-for g_ind, glom in enumerate(included_gloms):
-    for f_ind, filter_code in enumerate(filter_codes):
-        if f_ind == 0:
-            alpha = 1.0
-        else:
-            alpha = 0.5
-        for im_ind, image_name in enumerate(image_names):
-            ax[g_ind, im_ind].plot(all_responses[g_ind, im_ind, f_ind, :, :].mean(axis=0),
-                                   color=util.get_color_dict()[glom],
-                                   alpha=alpha)
 
+
+def get_vh_image(image_name):
+    return imread(os.path.join(images_dir, image_name))
+
+
+image_names = np.unique([x[0].replace('whitened_', '') for x in unique_parameter_values])
+filter_codes = np.unique([x[1] for x in unique_parameter_values])
+image_speeds = np.unique([x[2] for x in unique_parameter_values])
+
+
+for mean_response in all_responses:
+    fh, ax = plt.subplots(len(image_names), len(image_speeds)+1, gridspec_kw={'width_ratios': [1, 1, 1, 1, 1, 4]})
+    [plot_tools.cleanAxes(x) for x in ax.ravel()]
+    [x.set_ylim([-0.1, 0.75]) for x in ax[:, :-1].ravel()]
+    for im_ind, image_name in enumerate(image_names):
+        ax[im_ind, -1].imshow(get_vh_image(image_name), cmap='Greys_r')
+        for spd_ind, image_speed in enumerate(image_speeds):
+            for fc_ind, filter_code in enumerate(filter_codes):
+                pull_image_ind = np.where([image_name in x[0] for x in unique_parameter_values])[0]
+                pull_filter_ind = np.where([filter_code == x[1] for x in unique_parameter_values])[0]
+                pull_speed_ind = np.where([image_speed == x[2] for x in unique_parameter_values])[0]
+
+                pull_ind = list(set.intersection(set(pull_image_ind),
+                                                 set(pull_filter_ind),
+                                                 set(pull_speed_ind)))
+                assert len(pull_ind) == 1
+                pull_ind = pull_ind[0]
+
+                if filter_code == 0:
+                    alpha = 1.0
+                else:
+                    alpha = 0.5
+
+                ax[im_ind, spd_ind].plot(mean_response[glom_ind, pull_ind, :], 'k', alpha=alpha)
+                if im_ind == 0:
+                    ax[im_ind, spd_ind].set_title('{:.0f}'.format(image_speed),
+                                                  color='r' if image_speed==ID.getRunParameters('spot_speed') else 'k')
 
 # %%
 
--- scratch.py
+++ scratch.py
@@ -1,14 +1,72 @@
-from visanalysis.util import h5io
+from glom_pop import dataio
+from visanalysis.analysis import imaging_data
 import os
-import h5py
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
 
-data_dir = '/Users/mhturner/CurrentData/'
-file_path = os.path.join(data_dir, '2022-03-28.hdf5')
 
-with h5py.File(file_path, 'r+') as h5file:
+sync_dir = dataio.get_config_file()['sync_dir']
+data_directory = os.path.join(sync_dir, 'datafiles')
+video_dir = os.path.join(sync_dir, 'behavior_videos')
 
-    source = '/Flies/Fly1/'
-    dest = '/Flies/Fly2'
-    grp = h5file[source]
-    print(grp)
-    h5file.copy(source, dest, name='Fly2')
+# %%
+
+crops = [
+        ((120, 80), (150, 150), (0, 0)),
+         ]
+
+
+ds = ('20220404', 1)
+
+fh_tmp, ax_tmp = plt.subplots(len(crops)+1, 3, figsize=(12, 6))
+for c_ind, crop in enumerate(crops):
+    series_number = ds[1]
+    file_name = '{}-{}-{}.hdf5'.format(ds[0][:4], ds[0][4:6], ds[0][6:])
+
+    # For video:
+    series_dir = 'series' + str(series_number).zfill(3)
+    date_dir = ds[0]
+    file_path = os.path.join(data_directory, file_name)
+    ID = imaging_data.ImagingDataObject(file_path,
+                                        series_number,
+                                        quiet=True)
+
+    # Get video data:
+    # Timing command from voltage trace
+    voltage_trace, _, voltage_sample_rate = ID.getVoltageData()
+    frame_triggers = voltage_trace[0, :]  # First voltage trace is trigger out
+
+    video_filepath = glob.glob(os.path.join(video_dir, date_dir, series_dir) + "/*.avi")[0]  # should be just one .avi in there
+    video_results = dataio.get_ball_movement(video_filepath,
+                                             frame_triggers,
+                                             sample_rate=voltage_sample_rate,
+                                             cropping=crop)
+
+    # Show cropped ball and overall movement trace for QC
+    ax_tmp[c_ind, 0].plot(video_results['frame_times'], video_results['rmse'], 'k')
+    ax_tmp[c_ind, 1].hist(video_results['rmse'], 500)
+    ax_tmp[c_ind, 1].set_yscale('log')
+    ax_tmp[c_ind, 2].imshow(video_results['cropped_frame'], cmap='Greys_r')
+
+
+
+# %%
+
+
+fh, ax = plt.subplots(2, 1, figsize=(6, 3.5))
+ax[0].plot(video_results['rmse'], 'k')
+
+tw_ax = ax[1].twinx()
+tw_ax.fill_between(np.arange(len(video_results['binary_behavior'])), video_results['binary_behavior'], color='k', alpha=0.5)
+
+ax[1].plot(video_results['rmse'], 'b')
+# ax[1].set_xlim([3800, 4200])
+ax[1].axhline(video_results['binary_thresh'], color='r')
+
+
+
+
+
+
+# %%
