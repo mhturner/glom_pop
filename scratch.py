adb5dbdc367c84c31f3f58277e7c6543e8dc2033
Max Turner <maxwellholteturner@gmail.com>
2022-04-18 12:38:13 -0700

    dir. start one channel moco

--- scratch.py
+++ scratch.py
@@ -3,7 +3,9 @@ import os
 
 
 
-aa = '/oak/stanford/groups/trc/data/Max/Analysis/glom_pop/sync/tests1.dppd'
+aa = '/oak/stanford/groups/trc/data/Max/Analysis/glom_pop/sync/transforms/meanbrain_anatomical/TSeries-20220413-003/mean.nii'
 
-os.path.split(os.path.split(aa)[1])
-os.path.dirname(aa)
+os.path.dirname(aa)[-1]
+os.path.split(aa)[-1]
+
+os.path.split(os.path.dirname(aa))
