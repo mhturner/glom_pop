05ce03ff29f550bbeffa3d920216da0529972a65
Max Turner <maxwellholteturner@gmail.com>
2022-04-20 11:35:46 -0700

    behfig

--- glom_pop/pipeline.py
+++ glom_pop/pipeline.py
@@ -389,7 +389,7 @@ def save_behavior_fig(video_results, series_name, pipeline_dir):
                        color='k', alpha=0.5)
     ax[1].axhline(video_results['binary_thresh'], color='r')
     ax[1].plot(video_results['frame_times'][:200],
-               video_results['rmse'][[:200],
+               video_results['rmse'][:200],
                'b')
     ax[1].set_title(series_name)
 
