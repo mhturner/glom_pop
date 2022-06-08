0d67d80ddda827f6b356824d6afa897dd9810a11
Max Turner <maxwellholteturner@gmail.com>
2022-06-07 20:58:47 -0700

    continuing to mess w fictrac stuff

--- figs/repeat_spot_with_behavior.py
+++ figs/repeat_spot_with_behavior.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from visanalysis.analysis import imaging_data, shared_analysis
 from scipy.signal import resample, savgol_filter
 from scipy.stats import ttest_1samp
+from statsmodels.stats.multitest import multipletests
 from visanalysis.util import plot_tools
 import pandas as pd
 import glob
@@ -11,6 +12,8 @@ from skimage import filters
 
 from glom_pop import dataio, util, fictrac
 
+
+# TODO: check FT tracking for 04-12, 5. Got screwed up somehow...
 
 PROTOCOL_ID = 'ExpandingMovingSpot'
 
@@ -53,7 +56,6 @@ for series in matching_series:
 corr_with_running = []
 
 response_amps = []
-# running_amps = []
 walking_amps = []
 all_behaving = []
 
@@ -82,58 +84,44 @@ for s_ind, series in enumerate(matching_series):
     yrot = ft_data.iloc[:, 6] * 180 / np.pi * fps  # rot  --> deg/sec
     zrot = ft_data.iloc[:, 7] * 180 / np.pi * fps  # rot  --> deg/sec
 
-    xrot_filt = savgol_filter(xrot, 51, 3)
-    yrot_filt = savgol_filter(yrot, 51, 3)
-    zrot_filt = savgol_filter(zrot, 51, 3)
+    xrot_filt = savgol_filter(xrot, 151, 3)
+    yrot_filt = savgol_filter(yrot, 151, 3)
+    zrot_filt = savgol_filter(zrot, 151, 3)
 
-    walking_amp = np.sqrt(xrot_filt**2 + yrot_filt**2 + zrot_filt**2)
-    walking_amp_ds = resample(walking_amp, response_data.get('response').shape[1])
-    walking_amp_ds = savgol_filter(walking_amp_ds, 9, 3)
-    thresh = filters.threshold_li(walking_amp_ds)
-    binary_behavior_ds = (walking_amp_ds > thresh).astype('int')
+    walking_mag = np.sqrt(xrot_filt**2 + yrot_filt**2 + zrot_filt**2)
+    walking_mag_ds = resample(walking_mag, response_data.get('response').shape[1])
+    time_ds = ID.getAcquisitionMetadata('sample_period') * np.arange(response_data.get('response').shape[1])
+    thresh = filters.threshold_li(walking_mag_ds)
+    binary_behavior_ds = (walking_mag_ds > thresh).astype('int')
     _, behavior_binary_matrix = ID.getEpochResponseMatrix(binary_behavior_ds[np.newaxis, :],
                                                           dff=False)
-    #
-    # zrot_ds = resample(zrot_filt, response_data.get('response').shape[1])
-    _, walking_response_matrix = ID.getEpochResponseMatrix(walking_amp_ds[np.newaxis, :],
+    _, walking_response_matrix = ID.getEpochResponseMatrix(walking_mag_ds[np.newaxis, :],
                                                            dff=False)
 
     walking_amp = ID.getResponseAmplitude(walking_response_matrix, metric='mean')
+
     walking_amps.append(walking_amp)
-    #
-    # # Get behavior data
-    # behavior_data = dataio.load_behavior(ID, process_behavior=True)
-
-
-
-    # running_amps.append(behavior_data.get('running_amp'))
     response_amps.append(response_amp)
-    # all_behaving.append(behavior_data.get('behaving'))
 
-    # new_beh_corr = np.array([np.corrcoef(behavior_data.get('running_amp'), response_amp[x, :])[0, 1] for x in range(len(included_gloms))])
     new_beh_corr = np.array([np.corrcoef(walking_amp, response_amp[x, :])[0, 1] for x in range(len(included_gloms))])
     corr_with_running.append(new_beh_corr)
 
-
-
     # QC: check thresholding
-    # fh, ax = plt.subplots(1, 2, figsize=(8, 4))
-    # ax[0].plot(rmse_ds)
-    # ax[0].axhline(thresh, color='r')
-    # ax[1].hist(rmse_ds, 100)
-    # ax[1].axvline(thresh, color='r')
-    # ax[0].set_title('{}: {}'.format(file_name, series_number))
+    fh, ax = plt.subplots(1, 2, figsize=(8, 4))
+    ax[0].plot(walking_mag_ds)
+    ax[0].axhline(thresh, color='r')
+    ax[1].hist(walking_mag_ds, 100)
+    ax[1].axvline(thresh, color='r')
+    ax[0].set_title('{}: {}'.format(file_name, series_number))
 
     if np.logical_and(file_name == eg_series[0], series_number == eg_series[1]):
         # fh0: snippet of movement and glom response traces
-        fh0, ax0 = plt.subplots(3+len(included_gloms), 1, figsize=(5.5, 3.35))
+        fh0, ax0 = plt.subplots(2+len(included_gloms), 1, figsize=(5.5, 3.35))
         [x.set_ylim([y_min, y_max]) for x in ax0.ravel()]
         [util.clean_axes(x) for x in ax0.ravel()]
         [x.set_ylim() for x in ax0.ravel()]
 
         concat_response = np.concatenate([epoch_response_matrix[:, x, :] for x in eg_trials], axis=1)
-        # concat_running = np.concatenate([behavior_data.get('running_response_matrix')[:, x, :] for x in eg_trials], axis=1)
-        # concat_turning = np.concatenate([turning_response_matrix[:, x, :] for x in eg_trials], axis=1)
         concat_walking = np.concatenate([walking_response_matrix[:, x, :] for x in eg_trials], axis=1)
         concat_behaving = np.concatenate([behavior_binary_matrix[:, x, :] for x in eg_trials], axis=1)
         concat_time = np.arange(0, concat_walking.shape[1]) * ID.getAcquisitionMetadata('sample_period')
@@ -150,60 +138,57 @@ for s_ind, series in enumerate(matching_series):
                     'rv', markersize=4)
         ax0[0].set_ylim([0.25, 0.75])
         ax0[0].plot(concat_time, np.zeros_like(concat_time), color='w')
-        # ax0[0].set_ylabel('Stim', rotation=0)
 
         ax0[1].plot(concat_time, concat_walking[0, :], color='k')
         ax0[1].set_ylim([concat_walking.min(), concat_walking.max()])
         ax0[1].set_ylabel('Movement', rotation=0)
-        #
-        # ax0[2].plot(concat_time, concat_turning[0, :], color='k')
-        # ax0[2].set_ylim([concat_turning.min(), concat_turning.max()])
-        # ax0[2].set_ylabel('Rot.', rotation=0)
-        # plot_tools.addScaleBars(ax0[2], dT=4, dF=200, T_value=-2, F_value=-100)
 
         for g_ind, glom in enumerate(included_gloms):
-            ax0[3+g_ind].set_ylabel(glom, rotation=0)
-            ax0[3+g_ind].fill_between(concat_time, concat_behaving[0, :], color='k', alpha=0.25, linewidth=0)
-            ax0[3+g_ind].plot(concat_time, concat_response[g_ind, :], color=util.get_color_dict()[glom])
-            ax0[3+g_ind].set_ylim([concat_response.min(), concat_response.max()])
+            ax0[2+g_ind].set_ylabel(glom, rotation=0)
+            ax0[2+g_ind].fill_between(concat_time, concat_behaving[0, :], color='k', alpha=0.25, linewidth=0)
+            ax0[2+g_ind].plot(concat_time, concat_response[g_ind, :], color=util.get_color_dict()[glom])
+            ax0[2+g_ind].set_ylim([concat_response.min(), concat_response.max()])
             if g_ind == 0:
-                plot_tools.addScaleBars(ax0[3+g_ind], dT=4, dF=0.25, T_value=-1, F_value=-0.1)
+                plot_tools.addScaleBars(ax0[2+g_ind], dT=4, dF=0.25, T_value=-1, F_value=-0.1)
 
         # fh2: overall movement trace, with threshold and classification shading
-        fh2, ax2 = plt.subplots(2, 1, figsize=(4.5, 2))
+        fh2, ax2 = plt.subplots(1, 1, figsize=(4.5, 2))
 
-        ax2[0].set_ylabel('RMS image \ndiff. (a.u.)')
+        ax2.set_ylabel('Walking mag.')
 
-        tw_ax = ax2[0].twinx()
-        # tw_ax.fill_between(behavior_data['frame_times'][:len(behavior_data['binary_behavior'])],
-        #                    behavior_data['binary_behavior'],
-        #                    color=[0.5, 0.5, 0.5], alpha=0.5, linewidth=0.0)
-        # ax2[0].axhline(behavior_data['binary_thresh'], color='r')
-        # ax2[0].fill_between(behavior_data['frame_times'][:len(behavior_data['rmse_smooth'])],
-        #                  behavior_data['rmse_smooth'], y2=0,
-        #                  color='k')
-        # tw_ax.set_yticks([])
+        tw_ax = ax2.twinx()
+        tw_ax.fill_between(time_ds,
+                           binary_behavior_ds,
+                           color=[0.5, 0.5, 0.5], alpha=0.5, linewidth=0.0)
+        ax2.axhline(thresh, color='r')
+        ax2.fill_between(time_ds, walking_mag_ds, y2=0,
+                         color='k')
+        tw_ax.set_yticks([])
 
-
-        # ax2[1].plot(behavior_data['frame_times'][:zrot_filt.shape[0]], zrot_filt, color='k')
-        # ax2[1].set_ylabel('Rot.\n($\degree$/sec)')
-        # ax2[1].set_xlabel('Time (s)')
-        # ax2[1].set_ylim([-250, 250])
-        # ax2[1].set_yticks([-200, 0, 200])
-        # ax2[0].set_xticks([])
+        ax2.set_xlabel('Time (s)')
 
 corr_with_running = np.vstack(corr_with_running)  # flies x gloms
-# running_amps = np.vstack(running_amps)  # flies x trials
-# turning_amps = np.vstack(turning_amps)  # flies x trials
+walking_amps = np.vstack(walking_amps)  # flies x trials
 response_amps = np.dstack(response_amps)  # gloms x trials x flies
 
-[x.spines['top'].set_visible(False) for x in ax2]
-[x.spines['right'].set_visible(False) for x in ax2]
+ax2.spines['top'].set_visible(False)
+ax2.spines['right'].set_visible(False)
 tw_ax.spines['top'].set_visible(False)
 tw_ax.spines['right'].set_visible(False)
 
 # fh0.savefig(os.path.join(save_directory, 'repeat_beh_{}_resp.svg'.format(PROTOCOL_ID)), transparent=True)
 # fh2.savefig(os.path.join(save_directory, 'repeat_beh_{}_running.svg'.format(PROTOCOL_ID)), transparent=True)
+# %%
+
+walking_amp_ds = resample(walking_dist_ds, response_data.get('response').shape[1])
+# thresh = filters.threshold_li(walking_dist_ds)
+thresh = np.quantile(walking_dist_ds, 0.99)
+thresh
+fh, ax = plt.subplots(1, 1, figsize=(4, 4))
+ax.hist(walking_dist_ds, 100)
+ax.set_yscale('log')
+ax.axvline(x=thresh, color='r')
+
 
 # %% Corr w turning
 trial_gain = response_amps / np.nanmean(response_amps, axis=1)[:, np.newaxis, :]
@@ -247,11 +232,9 @@ ax2.invert_yaxis()
 
 p_vals = []
 for g_ind, glom in enumerate(included_gloms):
-    t_result = ttest_1samp(corr_with_running[:, g_ind], 0, nan_policy='omit')
+    t_result = ttest_1samp(corr_with_running[:, g_ind], popmean=0, nan_policy='omit')
     p_vals.append(t_result.pvalue)
 
-    if t_result.pvalue < (0.05 / len(included_gloms)):
-        ax2.annotate('*', (0.5, g_ind), fontsize=12)
 
     y_mean = np.nanmean(corr_with_running[:, g_ind])
     y_err = np.nanstd(corr_with_running[:, g_ind]) / np.sqrt(corr_with_running.shape[0])
@@ -263,6 +246,13 @@ for g_ind, glom in enumerate(included_gloms):
 
     ax2.plot([y_mean-y_err, y_mean+y_err], [g_ind, g_ind],
              color=util.get_color_dict()[glom])
+
+# Multiple comparisons test. Step down bonferroni
+h, p_corrected, _, _ = multipletests(p_vals, alpha=0.05, method='holm')
+for g_ind, glom in enumerate(included_gloms):
+    if h[g_ind]:
+        ax2.annotate('*', (0.5, g_ind), fontsize=12)
+
 
 ax2.set_yticks([])
 ax2.set_xlabel('Corr. with behavior (r)')
